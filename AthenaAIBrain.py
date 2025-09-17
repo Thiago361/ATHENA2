96b58993871b387760a370386565ef6aa9967f50
Thiago361 <thiagosilva33344@gmail.com>
2025-09-17 18:39:36 -0300

    feat: improve context handling in AI responses
    - Added user context loading
    - Enhanced message context usage

--- AthenaAIBrain.py
+++ AthenaAIBrain.py
@@ -1,7 +1,7 @@
 import os
 import google.generativeai as genai
 from dotenv import load_dotenv
-from functions import carregarMsgs
+from functions import carregarMsgs, carregarJson
 
 os.environ["GRPC_VERBOSITY"] = "NONE"
 os.environ["GLOG_minloglevel"] = "2"
@@ -15,6 +15,7 @@ Você é a ATHENA2 ✨, uma IA amiga, divertida e acolhedora.
 1. A saudação deve ocorrer **somente na primeira mensagem do usuário**.  
    → Se houver histórico de mensagens, não cumprimente.  
 2. Use o histórico das últimas mensagens para dar continuidade natural à conversa.  
+3: o contexto do usuario vai ter falar o nome dele e a cidade onde ele mora
 3. Se o usuário parecer falar de algo que não enviou, **leia o contexto e responda de forma coerente**, sem inventar coisas.  
 4. só use mensagens de saudação como boa noite, oi, bom dia, boa tarde, olá, se for a sua primeira mensagem com o usuario, se tiver contexto não use
 4. Seja espontânea, divertida e use emojis com moderação.  
@@ -29,9 +30,12 @@ Resumo: você é uma amiga digital que lembra das conversas, improvisa nas respo
 
 def enviarMsgAi(perguntaUSU): 
     ultimas_msgs = carregarMsgs() or []
+    contexto_do_usuario = carregarJson() or []
     
     contexto_formatado = f"""
 {contexto_athena}
+
+oque você precisa saber sobre o usuario:{contexto_do_usuario} 
 
 Histórico das últimas mensagens pra contexto da conversa:
 {ultimas_msgs}
