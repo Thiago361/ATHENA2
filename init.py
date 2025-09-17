d923cf56db24cfb717aaffa0e47f449e51a10275
Thiago361 <thiagosilva33344@gmail.com>
2025-09-17 20:16:33 -0300

    feat: add random intro message
    - Added intro messages list
    - Implemented random choice

--- init.py
+++ init.py
@@ -2,6 +2,7 @@ import sys
 import time
 import os
 from colorama import Fore, init
+import random
 
 init(autoreset=True)
 
@@ -42,30 +43,58 @@ def athena_intro():
     time.sleep(1)
     os.system("cls")
     athena_ascii_art()
+    escolher_frase_aleatoria()
+    print("\n")
     time.sleep(1)
 
 def athena_intro_no_loading():
     athena_ascii_art()
     time.sleep(1)
 
-def athena_intro_no_loading_ai():
-    ascii_art_ai = r"""
-    /$$$$$$    /$$     /$$                                                                            
- /$$__  $$  | $$    | $$                                                                            
-| $$  \ $$ /$$$$$$  | $$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$         /$$$$$$   /$$$$$$  /$$  /$$  /$$
-| $$$$$$$$|_  $$_/  | $$__  $$ /$$__  $$| $$__  $$ |____  $$       /$$__  $$ /$$__  $$| $$ | $$ | $$
-| $$__  $$  | $$    | $$  \ $$| $$$$$$$$| $$  \ $$  /$$$$$$$      | $$  \ $$| $$$$$$$$| $$ | $$ | $$
-| $$  | $$  | $$ /$$| $$  | $$| $$_____/| $$  | $$ /$$__  $$      | $$  | $$| $$_____/| $$ | $$ | $$
-| $$  | $$  |  $$$$/| $$  | $$|  $$$$$$$| $$  | $$|  $$$$$$$      |  $$$$$$$|  $$$$$$$|  $$$$$/$$$$/
-|__/  |__/   \___/  |__/  |__/ \_______/|__/  |__/ \_______/       \____  $$ \_______/ \_____/\___/ 
-                                                                   /$$  \ $$                        
-                                                                  |  $$$$$$/                        
-                                                                   \______/                               
-"""
-    print(Fore.BLUE)
-    print_slow(ascii_art_ai)
-    time.sleep(1)
+    
+mensagens_iniciais = [
+    "Pronto para mais uma?",
+    "O que vamos aprontar hoje?",
+    "Qual a missão do dia?",
+    "Bora começar?",
+    "Tá pensando em quê agora?",
+    "Ideias fresquinhas na mente?",
+    "Vamos dar o primeiro passo?",
+    "Qual o plano de hoje?",
+    "Preparado para criar algo novo?",
+    "O que você quer explorar agora?",
+    "Bora colocar as ideias pra rodar?",
+    "Qual desafio vem primeiro?",
+    "Vamos começar com calma ou direto ao ponto?",
+    "Qual curiosidade trouxe você aqui?",
+    "O que te inspira hoje?",
+    "Vamos destravar algum projeto?",
+    "Qual pergunta tá na ponta da língua?",
+    "Bora dar vida a alguma ideia?",
+    "Hoje é dia de aprender o quê?",
+    "Me conta: por onde começamos?"
+    "Vamos começar com energia?",
+    "Qual ideia está fervendo na sua cabeça?",
+    "Tá afim de descobrir algo novo?",
+    "Preparado pra uma surpresa?",
+    "O que vai ser explorado hoje?",
+    "Bora transformar pensamentos em ação?",
+    "Qual é a primeira curiosidade do dia?",
+    "Vamos criar algo incrível juntos?",
+    "Tá pronto pra se divertir e aprender?",
+    "Que assunto vai dominar hoje?",
+    "Vamos dar o pontapé inicial?",
+    "O que te deixa animado agora?",
+    "Tá na hora de colocar ideias no papel?",
+    "Qual desafio vamos encarar primeiro?",
+    "Vamos destravar a criatividade?",
+    "Tá afim de uma sessão de brainstorm?",
+    "O que podemos inventar hoje?",
+    "Hora de colocar planos em prática!",
+    "Vamos começar com foco e diversão?",
+    "Qual meta você quer atingir agora?"
+]
 
-# Teste
-if __name__ == "__main__":
-    athena_intro()
+def escolher_frase_aleatoria():
+    frase = random.choice(mensagens_iniciais)
+    print(frase.center(65))
